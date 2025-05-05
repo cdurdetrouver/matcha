8f05e272ee894c6a54860b5d94e5adaf0f9f0147
Pypypierre <easychinese76@gmail.com>
2025-05-05 15:38:17 +0200

    fix: pb when no user return only an empty array returned but not 404 nothing found like it should be

--- backend/src/routes/research.ts
+++ backend/src/routes/research.ts
@@ -56,9 +56,7 @@ app.post('/all', async (c: Context) => {
 	}
 
 	let users_list: User[];
-	if (user_lists.length == 0)
-		return c.json({ message: 'No users found' }, 404);
-	else if (user_lists.length == 1) {
+	if (user_lists.length == 1) {
 		users_list = user_lists[0];
 	} else {
 		users_list = user_lists.reduce((acc, list) => {
@@ -67,6 +65,8 @@ app.post('/all', async (c: Context) => {
 		});
 	}
 	users_list = users_list.filter((u) => u.id != user.id);
+	if (users_list.length == 0)
+		return c.json({ message: 'No users found' }, 404);
 	const users_list_s = await Promise.all(
 		users_list.map(async (user) => await user.serialize())
 	);

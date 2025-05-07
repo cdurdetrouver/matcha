d45571364562d5b4727f6a084336e8b2d66679ba
Pypypierre <easychinese76@gmail.com>
2025-05-07 10:36:57 +0200

    fix: protected create relation by check if the target user want this kind of rel

--- backend/src/routes/relations.ts
+++ backend/src/routes/relations.ts
@@ -75,6 +75,14 @@ app.post('/create', async (c: Context) => {
 	relation = Number(relation);
 	if (relation !== 0 && relation !== 2)
 		return c.json({ message: 'Relation range is invalid' }, 400);
+	if (target_user.wanted !== 0 && target_user.wanted !== relation)
+		return c.json(
+			{
+				message:
+					'Target user is not looking for this kind of relation',
+			},
+			400
+		);
 	if (await Relations_Users.is_related(user.id, target_id) != -1)
 		return c.json({ message: 'Relation already exists' }, 400);
 	if (relation === 0 && (target_user.wanted > 1 || user.wanted > 1))
@@ -156,35 +164,6 @@ app.delete('/delete', async (c: Context) => {
 });
 
 app.all('/delete', (c: Context) => {
-	return c.json({ message: 'Method Not Allowed' }, 405);
-});
-
-app.get('/is_related_to_me/:id', async (c: Context) => {
-	const ret_check = await check_cookies(c);
-	if (ret_check == null)
-		return c.json({ message: 'Server cannot perform checks !' }, 404);
-	const { message, user } = ret_check;
-	if (message != undefined || user == null)
-		return c.json({ message: message }, 401);
-	const id = Number(c.req.param('id'));
-	if (id == undefined) return c.json({ message: 'ID is required' }, 400);
-	if (id === user.id)
-		return c.json({ message: 'You cannot relate to yourself' }, 400);
-	try {
-		const target_user = await user.get_by_id(id);
-		const rel = await Relations_Users.is_related(user.id, id);
-		if (rel == -1) return c.json({ message: 
-			('User is not related to ' + target_user.username)}, 200);
-		return c.json({ message: ('User is related to '
-			+ target_user.username), relation: rel }, 200);
-	} catch (e) {
-		if (e instanceof Error && e.message === 'User not found')
-			return c.json({ message: 'User does not exist' }, 400);
-		return c.json({ message: 'Relation does not exist' }, 400);
-	}
-});
-
-app.all('/is_related_to_me/:id', (c: Context) => {
 	return c.json({ message: 'Method Not Allowed' }, 405);
 });
 
